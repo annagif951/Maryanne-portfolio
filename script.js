84299675dcec7add7ab563f64bf62e4877d6ae10
annagif951 <wairimumaryanne553@gmail .com>
2025-03-30 01:29:37 +0300

    first commits

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const greeting = document.createElement("p");
+    greeting.textContent = "Thanks for visiting my portfolio!";
+    document.body.appendChild(greeting);
+  });
+  
